4d6541a740b5454c512d22c0ad324b7766bb9886
Harish M <55819957+Harish3055@users.noreply.github.com>
2021-08-02 22:06:06 +0530

    Add files via upload

---
+++ PoseEstimationStream.py
@@ -0,0 +1,21 @@
+import cv2
+import numpy as np
+from PIL import ImageTk, Image
+import close
+
+class Pose_Estimation_Stream():
+	def __init__(self,cap,lmain):
+		self.lmain = lmain
+		self.cap   = cap
+	def Process(self):
+		_,frame = self.cap.read()
+		if frame is not None:
+			frame = cv2.resize(frame, (750, 590))
+			frame = cv2.cvtColor(frame,cv2.COLOR_BGR2GRAY)
+			cv2image = cv2.cvtColor(frame, cv2.COLOR_BGR2RGBA)
+			img = Image.fromarray(cv2image)
+			imgtk = ImageTk.PhotoImage(image=img)
+			self.lmain.imgtk = imgtk
+			self.lmain.configure(image=imgtk)
+			self.lmain.after(1, lambda:self.Process())		
+
